c4c6a925ed0ddb9d6166f2bbae186608cbc8930d
Darius Vainilavičius <radiocool@gmail.com>
2014-02-07 23:32:23 +0200

    table on lcd added

--- LCD_temp_humidity.ino
+++ LCD_temp_humidity.ino
@@ -50,10 +50,18 @@ void loop() {
   
   humidex = calculate_humidex (temperature, humidity);
 
+ 
+  
+  // Table 
+  tft.drawRect(0, 0, 128, 160, ST7735_WHITE);
+  tft.drawLine(0, 50, 128, 50, ST7735_WHITE);
+  tft.drawLine(0, 100, 128, 100, ST7735_WHITE);
+ 
   // data is outputed
-  temperature_to_lcd (temperature, 0);
+  
+  temperature_to_lcd (temperature, 4);
   humidity_to_lcd (humidity, 55);
-  humidex_to_lcd (humidex, 110);
+  humidex_to_lcd (humidex, 105);
 }
 
 // outputs temperature to LCD
@@ -61,7 +69,7 @@ void temperature_to_lcd (float temperature, unsigned char text_position )
 {
    int text_color;
  
-  tft.setCursor(0,text_position);       
+  tft.setCursor(4,text_position);       
   tft.setTextColor(ST7735_WHITE,ST7735_BLACK);
   tft.setTextSize(1);
   tft.print("Temperature:");
@@ -77,7 +85,7 @@ void temperature_to_lcd (float temperature, unsigned char text_position )
   }
 
 
-  tft.setCursor(0,text_position+20);
+  tft.setCursor(1,text_position+20);
   fix_number_position(temperature);
   tft.setTextColor(text_color,ST7735_BLACK);
   tft.print(temperature,1);
@@ -96,11 +104,11 @@ void humidity_to_lcd (float humidity, unsigned char text_position )
 
 
   tft.setTextColor(ST7735_WHITE,ST7735_BLACK);
-  tft.setCursor(0,text_position); 
+  tft.setCursor(4,text_position); 
   tft.setTextSize(1); 
   tft.println("Humidity:");
   tft.setTextSize(3);
-  tft.setCursor(0,text_position+20);
+  tft.setCursor(1,text_position+20);
 
   fix_number_position(humidity);
 
@@ -114,13 +122,13 @@ void humidity_to_lcd (float humidity, unsigned char text_position )
 void humidex_to_lcd (float humidex, unsigned char text_position )
 
 {
-  tft.setCursor(0,text_position); 
+  tft.setCursor(4,text_position); 
 
   tft.setTextSize(1); 
   tft.println("Humidex:");
   tft.setTextSize(3);
 
-  tft.setCursor(0,text_position+17);
+  tft.setCursor(1,text_position+17);
 
   
 
@@ -135,9 +143,10 @@ void humidex_to_lcd (float humidex, unsigned char text_position )
   tft.setCursor(108,text_position+17);
   tft.print("C"); 
   tft.drawChar(90,text_position+17, 247, text_color_humidex, ST7735_BLACK, 2); //degree symbol
-
-    tft.setTextSize(1); 
-    tft.println(message);
+    
+    tft.setCursor(3,text_position+43);
+    tft.setTextSize(1);
+    tft.print(message);
   }
 
 
@@ -145,9 +154,10 @@ void humidex_to_lcd (float humidex, unsigned char text_position )
   tft.print(" --.-"); 
   tft.setCursor(108,text_position+17);
   tft.print("C"); 
-  tft.drawChar(90,text_position+17, 247, ST7735_WHITE, ST7735_BLACK, 2); //degree symbol
-    
-    tft.println("                             "); 
+    tft.drawChar(90,text_position+17, 247, ST7735_WHITE, ST7735_BLACK, 2); //degree symbol
+     tft.setCursor(1,text_position+43);
+     tft.setTextSize(1);
+    tft.println("                   "); 
   };
 
 }
@@ -203,13 +213,13 @@ void get_humidex_color_warning_message(float humidex)
   if ((humidex >= 21 )&&(humidex < 27))
   {
     text_color_humidex=tft.Color565(0, 137, 0);
-    message= "No discomfort        ";
+    message= "No discomfort      ";
   } // dark green
 
   if ((humidex >= 27 )&&(humidex < 35))
   {
     text_color_humidex=tft.Color565(76, 255, 0); // light green
-    message= "Some discomfort      ";
+    message= "Some discomfort     ";
   }
 
   if ((humidex >= 35 )&&(humidex < 40))
